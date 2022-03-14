241aedb0f877077b104b228f982f8b051869d4fe
AngelynDisguise <angelyn.domingo122@gmail.com>
2022-03-14 21:10:42 +0000

    Add react-pdf

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -3,23 +3,22 @@ import React from 'react';
 function Home() {
   return (
     <div className="wrapper">
-      <div className="banner">
-        <h1>Angelyn Domingo</h1>
-        <h3>coder, organist, learner</h3>
-      </div>
-      <div className="about-me">
-    <p>
-        I am a Filipina and a first-generation college student. 
-        I am currently a sophomore majoring in computer science at George Mason University with an interest in 
-        web development, software engineering, and human-computer interaction.
-        Besides computing, I also have a passion for music and enjoy playing the piano/organ for my church.
-        When I am not coding or studying, I am usually playing Genshin Impact or watching anime with my family 
-        and friends.
-    </p>
-      </div>
-      <div className="education">
-
-      </div>
+        <div className="banner">
+            <h1>Angelyn Domingo</h1>
+            <h3>coder, organist, learner</h3>
+        </div>
+        <div className="about-me">
+            <p>
+                I am a Filipina and a first-generation college student. 
+                I am currently a sophomore majoring in computer science at George Mason University with an interest in 
+                web development, software engineering, and human-computer interaction.
+                Besides computing, I also have a passion for music and enjoy playing the piano/organ for my church.
+                When I am not coding or studying, I am usually playing Genshin Impact or watching anime with my family 
+                and friends.
+            </p>
+        </div>
+        <div className="resume">
+        </div> 
     </div>
   );
 }
