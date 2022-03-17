70619092abbfaae84165efa09647d5c1309129bc
AngelynDisguise <angelyn.domingo122@gmail.com>
2022-03-17 18:20:37 +0000

    Add resume and pfp

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf';
+import { Document, Page, pdfjs } from "react-pdf";
 
-import Resume from '../Angelyn_s_CV (edited 3-14-22).pdf';
+import Resume from '../Resume.pdf';
+import PFP from  '../PFP.jpg';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 
 function Home() {
   return (
     <div className="wrapper">
-        <div className="banner">
-            <h1>Angelyn Domingo</h1>
-            <h3>coder, organist, learner</h3>
+        <div className="pfp">
+            <img src={PFP} className="pfp" alt="Angelyn's profile" />
         </div>
         <div className="about-me">
+            <div className="banner">
+                <h1>Angelyn Domingo</h1>
+                <h3>coder, organist, learner</h3>
+            </div>
             <p>
                 I am a Filipina and a first-generation college student. 
                 I am currently a sophomore majoring in computer science at George Mason University with an interest in 
@@ -21,7 +28,11 @@ function Home() {
             </p>
         </div>
         <div className="resume">
-        <Document file={Resume}>
+        <Document 
+            file={Resume} 
+            options={{workerSrc: "pdf.worker.js"}}
+            onLoadError={console.error}
+        >
             <Page pageNumber={1} />
         </Document>
         </div> 
